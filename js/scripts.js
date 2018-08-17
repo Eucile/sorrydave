6cc68054f261f8cef39de1736e2060cf01dc8636
Krystal Foster <kmfoster777@gmail.com>
2018-08-17 09:48:05 -0700

    add && condition to account for 0s in numbers divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,18 @@ $(document).ready(function() {
   $("#formOne").submit(function() {
   event.preventDefault();
     var numberInput = parseInt($("input#userNumber").val());
-
     var robotString = '';
 
-    for (var i = 0; i < numberInput.length; i++) {
-    
-      if (numberInput % 3 === 0) {
+    // for (var i = 0; i < numberInput; i++) {
+
+      if ((numberInput % 3 === 0) && (numberInput === !0)) {
         robotString += "I'm sorry, Dave, I can't do that.";
       } else if (numberInput === 1) {
         robotString += "Boop!";
       } else if (numberInput === 0) {
         robotString += "Beep!";
       }
-    }
+    // }
 
     $(".answer").text(robotString);
 });
