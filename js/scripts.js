1d1955b81f33f31cf08bf08b335a46ff37631634
Krystal Foster <kmfoster777@gmail.com>
2018-08-17 14:35:11 -0700

    correct indentation in if statements

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,15 @@ $(document).ready(function() {
 
   if ((numberInput % 3 === 0) && (numberInput > 0)) {
     robotString += "I'm sorry, Dave, I can't do that.";
-
-    } else if (numberInput.toString().includes('1')) {
-      robotString += "Boop!";
-    } else if (numberInput.toString().includes('0')) {
-      robotString += "Beep!";
-    } else {
-      for (var i = numberInput - 1; i >= 0; i--) {
-        robotString += i;
-      }
+  } else if (numberInput.toString().includes('1')) {
+    robotString += "Boop!";
+  } else if (numberInput.toString().includes('0')) {
+    robotString += "Beep!";
+  } else {
+    for (var i = numberInput - 1; i >= 0; i--) {
+      robotString += i;
     }
+  }
 
   $(".answer").text(robotString);
   });
