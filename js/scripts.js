92f524e8824f5d8f86815b57a22c82fb012fcd04
Krystal Foster <kmfoster777@gmail.com>
2018-08-17 11:46:41 -0700

    remove 0, 1, and divisible if statements from for loop, and begin working on range values within for loop

--- js/scripts.js
+++ js/scripts.js
@@ -6,21 +6,36 @@ $(document).ready(function() {
     var numArray = [];
 
 
-    // for (var i = 0; i < numberInput; i++) {
-    //
-    //   numberIndex = numArray.push(numberInput[i]);
 
-      if ((numberInput % 3 === 0) && (numberInput > 0)) {
-        robotString += "I'm sorry, Dave, I can't do that.";
-      } else if (numberInput === 1) {
+    if ((numberInput % 3 === 0) && (numberInput > 0)) {
+      robotString += "I'm sorry, Dave, I can't do that.";
+
+      } else if (numberInput.toString().includes('1')) {
         robotString += "Boop!";
-      } else if (numberInput === 0) {
+      } else if (numberInput.toString().includes('0')) {
         robotString += "Beep!";
+      } else {
+
+        for (i = 0; i <= numberInput; i++) {
+          var numberIndex = numberInput[i];
+          robotString += numberIndex;
+        }
       }
-      // } else {
-      //   robotString += numberIndex;
-      // }
-    // }
+
+  // for (var i = 0; i < numberInput; i++) {
+  //     numberIndex = numberInput[i]);
+  //
+  //     if ((numberInput % 3 === 0) && (numberInput > 0)) {
+  //       robotString += "I'm sorry, Dave, I can't do that.";
+  //     } else if (numberIndex === 1) {
+  //       robotString += "Boop!";
+  //     } else if (numberIndex === 0) {
+  //       robotString += "Beep!";
+  //     }
+  //     // } else {
+  //     //   robotString += numberIndex;
+  //     // }
+  //  }
 
 $(".answer").text(robotString);
 });
