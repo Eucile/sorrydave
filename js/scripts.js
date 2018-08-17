d9da3924dc66fafbe81ae83ce4ec5b843a463b91
Krystal Foster <kmfoster777@gmail.com>
2018-08-17 15:16:36 -0700

    fix indentation in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,22 @@
 $(document).ready(function() {
   $("#formOne").submit(function() {
-  event.preventDefault();
-  $(".well").show();
-  var numberInput = parseInt($("input#userNumber").val());
-  var robotString = '';
+    event.preventDefault();
+    $(".well").show();
+    var numberInput = parseInt($("input#userNumber").val());
+    var robotString = '';
 
-  if ((numberInput % 3 === 0) && (numberInput > 0)) {
-    robotString += "I'm sorry, Dave, I can't do that.";
-  } else if (numberInput.toString().includes('1')) {
-    robotString += "Boop!";
-  } else if (numberInput.toString().includes('0')) {
-    robotString += "Beep!";
-  } else {
-    for (var i = numberInput - 1; i >= 0; i--) {
-      robotString += i;
+    if ((numberInput % 3 === 0) && (numberInput > 0)) {
+      robotString += "I'm sorry, Dave, I can't do that.";
+    } else if (numberInput.toString().includes('1')) {
+      robotString += "Boop!";
+    } else if (numberInput.toString().includes('0')) {
+      robotString += "Beep!";
+    } else {
+      for (var i = numberInput - 1; i >= 0; i--) {
+        robotString += i;
+      }
     }
-  }
 
-  $(".answer").text(robotString);
-  span.scrollIntoView();
+    $(".answer").text(robotString);
   });
 });
