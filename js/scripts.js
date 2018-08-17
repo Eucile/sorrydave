b98ccd12ade827be72570e061395c8db83cc3747
Krystal Foster <kmfoster777@gmail.com>
2018-08-17 14:29:21 -0700

    correct indentation and spacing in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,22 @@ $(document).ready(function() {
   $("#formOne").submit(function() {
   event.preventDefault();
   $(".well").show();
-
   var numberInput = parseInt($("input#userNumber").val());
   var robotString = '';
 
+  if ((numberInput % 3 === 0) && (numberInput > 0)) {
+    robotString += "I'm sorry, Dave, I can't do that.";
 
-    if ((numberInput % 3 === 0) && (numberInput > 0)) {
-      robotString += "I'm sorry, Dave, I can't do that.";
-
-      } else if (numberInput.toString().includes('1')) {
-        robotString += "Boop!";
-      } else if (numberInput.toString().includes('0')) {
-        robotString += "Beep!";
-      } else {
-        for (var i = numberInput - 1; i >= 0; i--) {
-          robotString += i;
-        }
+    } else if (numberInput.toString().includes('1')) {
+      robotString += "Boop!";
+    } else if (numberInput.toString().includes('0')) {
+      robotString += "Beep!";
+    } else {
+      for (var i = numberInput - 1; i >= 0; i--) {
+        robotString += i;
       }
+    }
 
   $(".answer").text(robotString);
-
   });
 });
