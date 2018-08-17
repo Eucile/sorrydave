bea767b4d1b03a0f334551e3a0b6d605fa2c0bba
Krystal Foster <kmfoster777@gmail.com>
2018-08-17 10:59:45 -0700

    set up push method to push unspecified digits back to user as a range

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,24 @@ $(document).ready(function() {
   event.preventDefault();
     var numberInput = parseInt($("input#userNumber").val());
     var robotString = '';
+    var numArray = [];
 
-    // for (var i = 0; i < numberInput; i++) {
 
-      if ((numberInput % 3 === 0) && (numberInput === !0)) {
+    for (var i = 0; i < numberInput; i++) {
+
+      numberIndex = numArray.push(numberInput[i]);
+
+      if ((numberInput % 3 === 0) && (numberInput > 0)) {
         robotString += "I'm sorry, Dave, I can't do that.";
       } else if (numberInput === 1) {
         robotString += "Boop!";
       } else if (numberInput === 0) {
         robotString += "Beep!";
+      } else {
+        robotString += numberIndex;
       }
-    // }
+    }
 
-    $(".answer").text(robotString);
+$(".answer").text(robotString);
 });
 });
