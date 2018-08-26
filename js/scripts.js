8866aeb30ca2cb1c8233e4759552cef8368cb501
Krystal Foster <kmfoster777@gmail.com>
2018-08-25 19:08:32 -0700

    update commented out code with same changes

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@
 // function getRobotString(numberInput) {
 // var robotString = '';
 
+// if (!Number.isInteger(parseInt(numberInput))) {
+//   alert("Please enter a number, Dave.")
+//   return "";
+// }
+// number = parseInt(numberInput)
+
 // for (var i = numberInput - 1; i >= 0; i--) {
 // if ((i % 3 === 0) && (i > 0)) {
 //   robotString += "I'm sorry, Dave, I can't do that.";
@@ -11,10 +17,18 @@
 // } else if (i.toString().includes('0')) {
 //   robotString += "Beep!";
 // } else {
-//   robotString += i;
+//   robotString += i + "," + " ";
 //   }
 // } return robotString
 // }
+
+// $(document).ready(function() {
+//   $("#formOne").submit(function() {
+//     event.preventDefault();
+//     $(".well").fadeIn(2000);
+//     $(".answer").text(getRobotString($("input#userNumber").val()));
+//   });
+// });
 
 //The active code below replaces only 1's and 0s appearing in the range with beep boops, but only returns the single "Sorry Dave" when the inputted whole number is divisible by 3. Otherwise, it always returns the range, just with beeps and boops for 0's and 1's
 
@@ -25,7 +39,7 @@ if (!Number.isInteger(parseInt(numberInput))) {
   alert("Please enter a number, Dave.")
   return "";
 }
-number = parseInt(numberInput)
+var number = parseInt(numberInput)
 
 if ((numberInput % 3 === 0) && (numberInput > 0)) {
   robotString += "I'm sorry, Dave, I can't do that.";
