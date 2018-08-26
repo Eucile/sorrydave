42a6edf36a06b5d5cde1f693fb69f4cabc26faa2
Krystal Foster <kmfoster777@gmail.com>
2018-08-25 18:18:52 -0700

    separated business and ui logic, moved for loop to perform output as directed

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,44 @@
+function getRobotString(numberInput) {
+var robotString = '';
+
+// This code would replace all numbers divisible by 3 with multi "I'm sorry Dave"'s' as well as the 1's and 0's with the specified strings
+
+// for (var i = numberInput - 1; i >= 0; i--) {
+// if ((i % 3 === 0) && (i > 0)) {
+//   robotString += "I'm sorry, Dave, I can't do that.";
+// } else if (i.toString().includes('1')) {
+//   robotString += "Boop!";
+// } else if (i.toString().includes('0')) {
+//   robotString += "Beep!";
+// } else {
+//   robotString += i;
+//   }
+// } return robotString
+// }
+
+//The active code below replaces only 1's and 0s with beep boops, but only returns the single "Sorry Dave" when the inputted whole number is divisible by 3. Otherwise, it always returns the range, just with beeps and boops for 0's and 1's
+
+if ((numberInput % 3 === 0) && (numberInput > 0)) {
+  robotString += "I'm sorry, Dave, I can't do that.";
+} else {
+    for (var i = numberInput - 1; i >= 0; i--) {
+      if (i.toString().includes('1')) {
+        robotString += "Boop!";
+      } else if (i.toString().includes('0')) {
+        robotString += "Beep!";
+      } else {
+        robotString += i;
+      }
+    }
+}
+return robotString
+}
+
+
 $(document).ready(function() {
   $("#formOne").submit(function() {
     event.preventDefault();
     $(".well").fadeIn(2000);
-    var numberInput = parseInt($("input#userNumber").val());
-    var robotString = '';
-
-    if ((numberInput % 3 === 0) && (numberInput > 0)) {
-      robotString += "I'm sorry, Dave, I can't do that.";
-    } else if (numberInput.toString().includes('1')) {
-      robotString += "Boop!";
-    } else if (numberInput.toString().includes('0')) {
-      robotString += "Beep!";
-    } else {
-      for (var i = numberInput - 1; i >= 0; i--) {
-        robotString += i;
-      }
-    }
-
-    $(".answer").text(robotString);
+    $(".answer").text(getRobotString(parseInt($("input#userNumber").val())));
   });
 });
